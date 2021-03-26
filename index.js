8fe71648d5b18b7a9bcac3a77a17d754e530fcbf
DariDiniz <darianydiniz@gmail.com>
2021-03-26 12:05:37 -0300

    Add chamada de dadosPets.son e retirada da lista do index.js

--- index.js
+++ index.js
@@ -1,41 +1,9 @@
 const moment = require('moment');
+let dadospet = require('./dadosPets.json');
 
 const nomePetshop = "PETSHOP AVANADE"
 
-let pets = [{
-    nome: "Lady Juju",
-    tipo: "cachorro",
-    idade: 0.3,
-    raca: "SRD",
-    peso: 3,
-    tutor: "Dari",
-    contato: "(81) 98859-5324",
-    vacinado: true,
-    servicos: ["banho", "tosa"]
-},
-{
-    nome: "Snoopy",
-    tipo: "cachorro",
-    idade: 13,
-    raca: "Beagle",
-    peso: 3,
-    tutor: "Charlie Brown",
-    contato: "(11) 98374-9573",
-    vacinado: false,
-    servicos: ["banho"]
-},
-{
-    nome: "Salem",
-    tipo: "gato",
-    idade: 100,
-    raca: "SRD",
-    peso: 3,
-    tutor: "Sabrina",
-    contato: "(21) 99573-9473",
-    vacinado: false,
-    servicos: ["banho"]
-}
-];
+
 
 const listarPets = () => {
     for(let pet of pets){
